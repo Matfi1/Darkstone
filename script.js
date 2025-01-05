1171bfd8984b80b1cc94e1ff4fec7418bb6ad6e1
Matfi1 <matfi.yakovenko100@gmail.com>
2025-01-05 13:54:41 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+document.addEventListener('scroll', () => {
+    const sections = document.querySelectorAll('.section');
+    sections.forEach(section => {
+        const sectionTop = section.getBoundingClientRect().top;
+        if (sectionTop < window.innerHeight - 100) {
+            section.classList.add('visible');
+        }
+    });
+});
+
+// Прокрутка к разделам по клику на меню
+const menuLinks = document.querySelectorAll('.menu a');
+
+menuLinks.forEach(link => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        const targetId = link.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+
+        window.scrollTo({
+            top: targetSection.offsetTop - 20,
+            behavior: 'smooth'
+        });
+    });
+});
