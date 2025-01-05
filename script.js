cc28a71cc6e9bc62286639d8f243e133fe62fefc
Matfi1 <matfi.yakovenko100@gmail.com>
2025-01-05 13:54:19 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('scroll', () => {
-    const sections = document.querySelectorAll('.section');
-    sections.forEach(section => {
-        const sectionTop = section.getBoundingClientRect().top;
-        if (sectionTop < window.innerHeight - 100) {
-            section.classList.add('visible');
-        }
-    });
-});
